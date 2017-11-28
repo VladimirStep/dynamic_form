67c0152e19932be6886b305015c70f4d02f2fbeb
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-11-28 09:57:24 +0200

    Add main component and add redux, react-redux, redux-form to project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import './index.css';
+import DynamicFormApp from './presentations/dynamic_form_app';
+
+const allRedusers = {
+    form: formReducer
+};
+const rootReduser = combineReducers(allRedusers);
+const store = createStore(rootReduser);
 
 ReactDOM.render(
-    null,
+    <Provider store={ store }>
+        <DynamicFormApp />
+    </Provider>,
     document.getElementById('root')
 );
---
+++ src/presentations/dynamic_form_app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function DynamicFormApp() {
+    return null;
+}
+
+export default DynamicFormApp;
