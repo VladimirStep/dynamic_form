adf89bf2d52bd4f0d1614167479f225f977960b0
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-11-28 10:58:10 +0200

    Add basic form constructor

--- src/presentations/dynamic_form_app.js
+++ src/presentations/dynamic_form_app.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import FormConstructor from './form_constructor';
+import styled from 'styled-components';
+
+const AppWrapper = styled.main`
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    background-color: #ffffff;
+`;
 
 function DynamicFormApp() {
-    return null;
+    return (
+        <AppWrapper>
+            <FormConstructor />
+        </AppWrapper>
+    );
 }
 
 export default DynamicFormApp;
---
+++ src/presentations/form_constructor.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import styled from 'styled-components';
+
+function FormConstructor(props) {
+    const {handleSubmit, pristine, reset, submitting} = props;
+    const FormConstructorSection = styled.section`
+        width: 100%;
+        padding: 20px;
+        background-color: #eeeeee;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+    `;
+
+    return (
+        <FormConstructorSection>
+            <h3>Form Constructor</h3>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Field name</label>
+                    <div>
+                        <Field name='fieldName' component='input' type='text' />
+                    </div>
+                </div>
+                <div>
+                    <label>Field label</label>
+                    <div>
+                        <Field name='fieldLabel' component='input' type='text' />
+                    </div>
+                </div>
+                <div>
+                    <label>Field type</label>
+                    <div>
+                        <Field name='fieldType' component='select'>
+                            <option value="text input">text input</option>
+                            <option value="textarea input">textarea input</option>
+                            <option value="select">select</option>
+                        </Field>
+                    </div>
+                </div>
+                <div>
+                    <label htmlFor="isRequired">Required</label>
+                    <Field name='isrRequired' id='isrRequired' component='input' type='checkbox' />
+                </div>
+                <div>
+                    <button type='submit' disabled={pristine || submitting}>Add Field</button>
+                    <button type='button' disabled={pristine || submitting} onClick={reset}>Clear</button>
+                </div>
+            </form>
+        </FormConstructorSection>
+    );
+}
+
+export default reduxForm({
+    form: 'constructor'
+})(FormConstructor);
